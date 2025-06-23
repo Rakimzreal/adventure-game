e4dbb3896ceb701b05984f877f7926de64f875c3
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-06-23 11:25:06 +0200

    Add intro and player name prompt

---
+++ game.py
@@ -0,0 +1,6 @@
+name = input('Hi, what is your name: ')
+print(f'Hello {name}, welcome to my game! ')
+print('You wake up in a quiet forest clearing. You hear the sound of water in the distance and see two paths ahead of you.')
+print('Do you want to play a game? (yes/no)')
+play = input('> ').lower()
+
