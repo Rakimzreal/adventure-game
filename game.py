475cadb028ff05c7684746f10dffbb561034a067
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-06-23 12:01:33 +0200

    Set up right path with necessary options for the game, added some emojis

--- game.py
+++ game.py
@@ -6,15 +6,15 @@ play = input('> ').lower()
 
 if play == 'yes':
     print('game on! 😊')
-    print('You can go left toward the sound of water, or right into the dark woods. Where do you go? (left/right)' )
+    print('You can go left toward the sound of water, or right into the dark woods. Where do you go? (left/right) 🤔' )
     choice = input('> ').lower()
     if choice == 'left':
         print('You find a shaky rope bridge over a river.')
-        print('Do you want to cross it or turn back? (cross/ turn back)')
+        print('Do you want to cross it or turn back? (cross/ turn back) 🤔')
         choice = input('> ').lower()
         if choice == 'cross':
             print('The bridge creaks, but you make it across. You find a chest buried under a tree.')
-            print('Open the chest? (yes/no)')
+            print('Open the chest? (yes/no) 🤔')
             choice = input('> ').lower()
             if choice == 'yes':
                 print('🎉 You find a magical map that leads you out safely.')
@@ -22,5 +22,20 @@ if play == 'yes':
                 print(' A wild boar charges you for lingering. Game over. 😒')
         elif choice == 'turn back':
             print('A wild boar charges you for lingering. Game over. 😒')
+    if choice == 'right':
+        print('The woods get darker, and you stumble upon a sleeping bear. 😨')
+        print('Do you wanna sneak past it or run? (sneak / run) 🤔')
+        choice = input('> ').lower()
+        if choice == 'sneak':
+            print('You tiptoe around... success! You find a cave. 😁')
+            print('Enter cave? (yes/no) 🤔')
+            choice = input('> ').lower()
+            if choice == 'yes':
+                print('Inside is an old traveler who gives you safe passage. You survive. 🎉')
+            elif choice == 'no':
+                print('You get lost trying to backtrack. Bear attacks you. Game over. 😒')
+        elif choice == 'run':
+            print('You wake the bear. You already know how that ends. Game over! 🐻')
+            
 else:
-    print('We ae NOT playing...😔')
+    print('We are NOT playing...😔')
